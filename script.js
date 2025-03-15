be7ae6c1f0d71f8180834e5b17ae8f385afa4cfe
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2025-03-15 08:12:30 -0400

    Sums works

--- script.js
+++ script.js
@@ -3,125 +3,40 @@
 // elements
 const testButton = document.querySelector(`.btn--test`);
 const refreshButton = document.querySelector(`.btn--refresh`);
-const inputA = document.querySelector(`.input--A`);
-const inputB = document.querySelector(`.input--B`);
+const inputHelmet = document.querySelector(`.input--helmet`);
+const inputRings = document.querySelectorAll(`.input--ring`);
 const outputA = document.querySelector(`.output--A`);
+const outputB = document.querySelector(`.output--B`);
 
 const data = document.querySelector(".data");
 
-const identifications = [
-    "baseHealth",
-    "baseFireDefence",
-    "defence",
-    "rawDefence",
-    "xpBonus",
-    "lootBonus",
-    "fireDamage",
-    "thunderDefence",
-    "waterDefence",
-    "fireDefence",
-    "baseDamage",
-    "rawStrength",
-    "baseEarthDamage",
-    "strength",
-    "mainAttackDamage",
-    "spellDamage",
-    "thorns",
-    "walkSpeed",
-    "baseFireDamage",
-    "rawHealth",
-    "manaRegen",
-    "healingEfficiency",
-    "raw1stSpellCost",
-    "soulPointRegen",
-    "earthDefence",
-    "baseThunderDefence",
-    "baseAirDefence",
-    "agility",
-    "airDamage",
-    "rawAgility",
-    "manaSteal",
-    "elementalDamage",
-    "baseThunderDamage",
-    "dexterity",
-    "rawSpellDamage",
-    "thunderDamage",
-    "waterDamage",
-    "healthRegen",
-    "baseWaterDefence",
-    "intelligence",
-    "reflection",
-    "healthRegenRaw",
-    "poison",
-    "earthDamage",
-    "baseWaterDamage",
-    "baseAirDamage",
-    "rawIntelligence",
-    "1stSpellCost",
-    "2ndSpellCost",
-    "3rdSpellCost",
-    "4thSpellCost",
-    "rawDexterity",
-    "rawMainAttackDamage",
-    "baseEarthDefence",
-    "airDefence",
-    "lifeSteal",
-    "stealing",
-    "elementalDefence",
-    "sprint",
-    "sprintRegen",
-    "jumpHeight",
-    "rawAttackSpeed",
-    "rawFireMainAttackDamage",
-    "earthSpellDamage",
-    "rawEarthSpellDamage",
-    "exploding",
-    "fireSpellDamage",
-    "earthMainAttackDamage",
-    "waterSpellDamage",
-    "rawAirMainAttackDamage",
-    "raw2ndSpellCost",
-    "airSpellDamage",
-    "rawEarthMainAttackDamage",
-    "rawThunderMainAttackDamage",
-    "rawFireDamage",
-    "rawFireSpellDamage",
-    "raw4thSpellCost",
-    "raw3rdSpellCost",
-    "rawWaterSpellDamage",
-    "elementalMainAttackDamage",
-    "knockback",
-    "rawThunderSpellDamage",
-    "rawElementalSpellDamage",
-    "slowEnemy",
-    "elementalSpellDamage",
-    "rawElementalMainAttackDamage",
-    "rawDamage",
-    "damage",
-    "neutralDamage",
-    "rawNeutralDamage",
-    "healing",
-    "thunderSpellDamage",
-    "airMainAttackDamage",
-    "rawNeutralSpellDamage",
-    "rawAirSpellDamage",
-    "rawThunderDamage",
-    "rawWaterDamage",
-    "rawElementalDamage",
-    "thunderMainAttackDamage",
-    "fireMainAttackDamage",
-    "rawNeutralMainAttackDamage",
-    "rawEarthDamage",
-    "rawAirDamage",
-    "neutralMainAttackDamage",
-    "weakenEnemy",
-    "leveledXpBonus",
-    "damageFromMobs",
-    "leveledLootBonus",
-    "gatherXpBonus",
-    "rawWaterMainAttackDamage",
-    "gatherSpeed",
-    "lootQuality",
+const inputs = [
+    document.querySelector(`.input--helmet`),
+    document.querySelector(`.input--chestplate`),
+    document.querySelector(`.input--leggings`),
+    document.querySelector(`.input--boots`),
+    document.querySelector(`.input--ring1`),
+    document.querySelector(`.input--ring2`),
+    document.querySelector(`.input--bracelet`),
+    document.querySelector(`.input--necklace`),
+    document.querySelector(`.input--weapon`),
+];
+
+const item_types = [
+    "helmet",
+    "chestplate",
+    "leggings",
+    "boots",
+    "ring",
+    "ring",
+    "bracelet",
+    "necklace",
+    "relik",
+    "spear",
+    "wand",
+    "bow",
+    "dagger",
+    "weapons",
 ];
 
 const sound = new Audio("sounds/mythic_old.ogg");
@@ -130,61 +45,113 @@ testButton.addEventListener("click", function () {
 });
 
 refreshButton.addEventListener("click", function () {
-    setOutput();
+    addAllItemData();
 });
 
-//JSONs
-logData();
+const addAllItemData = function () {
 
-const findMatch = function () {
-    const helmet = JSON.parse(document.querySelector(`.data--helmet`).value)[inputA.value];
+    const combined = Object();
+    // loop through each input, adding each id to the combined
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        addItemToCombined(input, combined);
+        console.log('inputs.length = ' + inputs.length + ', combined = ' + JSON.stringify(combined))
+    }
 
-    if (helmet === undefined) return;
+    outputB.textContent = JSON.stringify(combined)
+};
 
-    const ids = Object.entries(helmet["identifications"]);
+const addItemToCombined = function (input, combined) {
+    console.log('start addItemToCombined');
+    const itemsString = document.querySelector(`.data--` + input.dataset["slot"]).value;
+    if (itemsString === undefined) return 'itemString is undefined';
+
+    const item = JSON.parse(itemsString)[input.value];
+    if (item === undefined) return 'item is undefined';
+
+    addCategory(combined, item, 'base');
+    addCategory(combined, item, 'identifications');
+}
+
+const addCategory = function(combined, item, subObjectName) {
+    if (item[subObjectName] === undefined) return;
+    const subObject = Object.entries(item[subObjectName]);
+
+    for (let i = 0; i < subObject.length; i++) {
+        const id = subObject[i];
+
+        var result = id[1];
+        if (!Number.isInteger(id[1])) {
+            result = result["max"];
+        }
+
+        if (combined[id[0]] === undefined) {
+            combined[id[0]] = result;
+        } else {
+            combined[id[0]] += result;
+        }
+    }
+}
+
+
+
+const addItemFromSlot = function (input) {
+    const inputString = document.querySelector(`.data--` + input.dataset["slot"]).value;
+    if (inputString === undefined) return '';
+
+    const item = JSON.parse(inputString)[input.value];
+    if (item === undefined) return '';
+    if (item["base"] === undefined) return '';
+    const base = Object.entries(item["base"]);
 
     var strungTogether = "";
+    for (let i = 0; i < base.length; i++) {
+        const id = base[i];
+
+        var result = id[1];
+        if (!Number.isInteger(id[1])) {
+            result = result["max"];
+        }
+
+        strungTogether += id[0] + ": " + JSON.stringify(result) + "\n";
+    }
+
+    const ids = Object.entries(item["identifications"]);
+
     for (let i = 0; i < ids.length; i++) {
-        const identification = ids[i];
-        strungTogether += JSON.stringify(ids[i]) + "\n";
+        const id = ids[i];
+
+        var result = id[1];
+        if (!Number.isInteger(id[1])) {
+            result = result["max"];
+        }
+
+        strungTogether += id[0] + ": " + JSON.stringify(result) + "\n";
     }
 
     return strungTogether;
-};
-
-const setOutput = function () {
-    outputA.textContent = findMatch();
 };
 
 // document.addEventListener() can be used to catch inputs on a global level
 // window.addEventListener() can be used to catch when the page fully loads, or wants to unload
 
 window.addEventListener("load", function () {
-    inputA.addEventListener("input", function () {
-        setOutput();
+    inputs.forEach(input => {
+        input.addEventListener("input", function () {
+            addAllItemData(input);
+        });
     });
 });
 
-async function logData() {
-    const item_types = [
-        "helmet",
-        "chestplate",
-        "leggings",
-        "boots",
-        "ring",
-        "bracelet",
-        "necklace",
-        "relik",
-        "spear",
-        "wand",
-        "bow",
-        "dagger",
-    ];
 
+// log the JSONs into HTML
+logData();
+async function logData() {
     for (let i = 0; i < item_types.length; i++) {
         const data = document.querySelector(".data--" + item_types[i]);
         data.value = JSON.stringify(await fetchData(item_types[i]));
     }
+    console.log('setup complete')
 }
 
 async function fetchData(file) {
@@ -201,3 +168,4 @@ async function fetchData(file) {
         console.error(error + " " + file);
     }
 }
+
