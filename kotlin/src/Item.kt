867790cc6038cec15331319b88c71d21253fe162
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2025-12-22 13:53:54 -0500

    Updated to new standard

--- kotlin/src/Item.kt
+++ kotlin/src/Item.kt
@@ -3,109 +3,68 @@ import file.readStringFromFile
 import file.writeStringToFile
 import org.json.JSONArray
 import org.json.JSONObject
+import java.lang.Integer.parseInt
+import kotlin.math.max
 
 fun item() {
     // get the new state of each item
-    val allItems = getJSONFromAPI("https://api.wynncraft.com/v3/item/database?fullResult")
-    transposeSubtyping(allItems)
-    addNameToItems(allItems)
+    val apiItems = getJSONFromAPI("https://api.wynncraft.com/v3/item/database?fullResult")
+    writeStringToFile("database/api/items.json", apiItems.toString())
 
-    val groups = getGroupArrays(allItems)
+    transposeSubtyping(apiItems)
+    addNameToItems(apiItems)
 
-    updateItemIndexes(allItems, groups)
+    val itemData = JSONObject(readStringFromFile("database/items.json"))
+    val groupData = JSONObject(readStringFromFile("database/groups.json"))
+    // major ids are in an array since the descriptions have to be manually updated, and this is just to auto-index them.
+    val majorIds = JSONArray(readStringFromFile("database/majorIds.json"))
 
-    val database = JSONObject(readStringFromFile("database/items.json"))
+    var nextIndex: Int = parseInt(
+        itemData.names()
+            .reduce { x, y ->
+                max(parseInt(x as String), parseInt(y as String)).toString()
+            } as String) + 1
+    for (fakeName in apiItems.names()) {
+        val item = apiItems.getJSONObject(fakeName as String)
+        val internalName = item.getString("internalName")
 
-    updateDatabase(allItems, database)
+        val id = parseInt(
+            itemData.names().find { id ->
+                itemData.getJSONObject(id as String).getString("internalName") == internalName
+            } as String?
+                ?: nextIndex.toString())
+        itemData.put(id.toString(), item)
+        if (id == nextIndex) nextIndex++
 
-    writeMaIds(allItems)
-    writeFiles(allItems, groups, database)
-}
+        for (groupTag in arrayOf("type", "subType")) {
+            if (!item.has(groupTag)) continue
+            val group = item.getString(groupTag)
+            if (!groupData.has(group)) groupData.put(group, JSONArray())
+            val groupArray = groupData.getJSONArray(group)
+            if (groupArray.indexOf(internalName) == -1) groupArray.put(internalName)
+        }
 
-fun writeFiles(newQuery: JSONObject, groups: JSONObject, database: JSONObject) {
-    writeStringToFile("database/formatted/items.json", newQuery.toString(2))
+        if (item.has("majorIds")) {
+            for (majorId in item.getJSONObject("majorIds").names())
+                if (majorIds.indexOf(majorId as String) == -1) majorIds.put(majorId)
 
-    writeStringToFile("database/indexes/item_groups.json", "$groups")
-    writeStringToFile("database/wynnsmith/indexed_names.js", "const indexedInternalNameGroups = $groups")
+            item.put("majorIds", item.getJSONObject("majorIds").names().map {name -> majorIds.indexOf(name)})
+        }
+    }
 
-    writeStringToFile("database/items.json", "$database")
-    writeStringToFile("database/wynnsmith/items.js", "const allItems = ${database.toJSONArray(database.names())}")
+    writeStringToFile("database/items.json", "$itemData")
+    writeStringToFile("database/js/items.js", "export default $itemData")
+    writeStringToFile("database/formatted/items.json", itemData.toString(2))
+    writeStringToFile("database/groups.json", "$groupData")
+    writeStringToFile("database/js/groups.js", "export default $groupData")
+    writeStringToFile("database/formatted/groups.json", groupData.toString(2))
+    writeStringToFile("database/majorIds.json", "$majorIds")
+    writeStringToFile("database/formatted/majorIds.json", majorIds.toString(2))
 }
 
 fun addNameToItems(allItems: JSONObject) {
-    for (itemName in allItems.names()) {
-        itemName as String
-        allItems.getJSONObject(itemName).put("name", itemName)
-    }
-}
-
-fun updateDatabase(allItems: JSONObject, database: JSONObject) {
-    for (itemName in allItems.names()) {
-        itemName as String
-        val item = allItems.getJSONObject(itemName)
-        val internalName = item.getString("internalName")
-        database.put(internalName, item)
-    }
-}
-
-fun getGroupArrays(allItems: JSONObject): JSONObject {
-    val groupArrays = JSONObject(readStringFromFile("database/indexes/item_groups.json"))
-
-    // find groups in the new data
-    val groupNames = JSONArray()
-    for (itemName in allItems.names()) {
-        val item = allItems.getJSONObject(itemName as String)
-        val type = item.getString("type")
-        if (groupNames.indexOf(type) == -1) groupNames.put(type)
-        if (item.has("subType")) {
-            val subType = item.getString("subType")
-            if (groupNames.indexOf(subType) == -1) groupNames.put(subType)
-        }
-    }
-
-    // any groups that aren't already in the database are added
-    for (groupName in groupNames)
-        if (!groupArrays.has(groupName as String)) groupArrays.put(groupName, JSONArray())
-
-    return groupArrays
-}
-
-fun updateItemIndexes(allItems: JSONObject, groups: JSONObject) {
-    // loop over all items, crosschecking them with their type and subType databases
-    for (itemName in allItems.names()) {
-        itemName as String
-        val item = allItems.getJSONObject(itemName)
-        val internalName = item.getString("internalName")
-
-        val type = item.getString("type")
-        val typeArray = groups.getJSONArray(type)
-        if (typeArray.indexOf(internalName) == -1)
-            typeArray.put(internalName)
-
-        if (item.has("subType")) {
-            val subType = item.getString("subType")
-            val subTypeArray = groups.getJSONArray(subType)
-            if (subTypeArray.indexOf(internalName) == -1)
-                subTypeArray.put(internalName)
-        }
-    }
-}
-
-fun writeMaIds(allItems: JSONObject) {
-    val maIds = JSONArray(readStringFromFile("database/maIds.json"))
-    for (name in allItems.names()) {
-        name as String
-        val item = allItems.getJSONObject(name)
-        if (!item.has("majorIds")) continue
-        val itemMaIds = item.getJSONObject("majorIds")
-        for (maId in itemMaIds.names()) {
-            maId as String
-            if (maIds.indexOf(maId) == -1) {
-                maIds.put(maId)
-            }
-        }
-    }
-    writeStringToFile("database/maIds.json", "$maIds")
+    for (itemName in allItems.names())
+        allItems.getJSONObject(itemName as String).put("name", itemName)
 }
 
 fun transposeSubtyping(allItems: JSONObject) {
--- kotlin/src/Tree.kt
+++ kotlin/src/Tree.kt
@@ -1,58 +1,16 @@
 import api.getJSONFromAPI
-import file.readStringFromFile
 import file.writeStringToFile
-import org.json.JSONArray
 import org.json.JSONObject
 
+val classNames = arrayOf("archer", "assassin", "mage", "shaman", "warrior")
+
 fun tree() {
-    writeTrees()
-    indexAspects()
-}
-
-val classNames = arrayOf("archer", "warrior", "assassin", "mage", "shaman")
-
-fun writeTrees(): JSONObject {
-    val classes = JSONObject()
-
     for (name in classNames) {
         val wynnClass = JSONObject()
         wynnClass.put("tree", getJSONFromAPI("https://api.wynncraft.com/v3/ability/tree/$name"))
-        wynnClass.put("map", dePageMap(getJSONFromAPI("https://api.wynncraft.com/v3/ability/map/$name")))
+        wynnClass.put("map", getJSONFromAPI("https://api.wynncraft.com/v3/ability/map/$name"))
         wynnClass.put("aspects", getJSONFromAPI("https://api.wynncraft.com/v3/aspects/$name"))
-        classes.put(name, wynnClass)
+
+        writeStringToFile("database/trees/$name.json", wynnClass.toString(2))
     }
-
-    writeStringToFile("database/formatted/class_trees.json", classes.toString(2))
-
-    return classes
-}
-
-fun dePageMap(map: JSONObject): JSONArray {
-    val unPagedMap = JSONArray()
-    for (i: Int in 0..<map.length()) {
-        val pageName = map.names().getString(i)
-        val page = map.getJSONArray(pageName)
-
-        for (j: Int in 0..<page.length()) {
-            unPagedMap.put(page.getJSONObject(j))
-        }
-    }
-    return unPagedMap
-}
-
-fun indexAspects() {
-    val aspectsDatabases = JSONObject(readStringFromFile("database/indexes/aspects.json"))
-
-    for (name in classNames) {
-        val classAspects = getJSONFromAPI("https://api.wynncraft.com/v3/aspects/$name")
-        val aspectsDatabase = aspectsDatabases.getJSONArray(name)
-
-        for (aspectName in classAspects.names()) {
-            if (aspectsDatabase.indexOf(aspectName) == -1)
-                aspectsDatabase.put(aspectName)
-        }
-    }
-
-    writeStringToFile("database/indexes/aspects.json", "$aspectsDatabases")
-    writeStringToFile("database/wynnsmith/indexed_aspects.js", "const aspect_indexes = $aspectsDatabases")
 }
