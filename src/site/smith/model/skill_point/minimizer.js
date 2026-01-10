f3d70e90c988f827011d7d6ae1c7163279207ddc
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2026-01-10 06:23:49 -0500

    Fixed for -SP, sped up significantly.

--- src/site/smith/model/skill_point/minimizer.js
+++ src/site/smith/model/skill_point/minimizer.js
@@ -3,19 +3,22 @@
 // run through properValidate, and if it works
 
 import permuteOrders from "../../../common/permutation.js";
-import {properValidate} from "./verifier.js";
+import {wynnValidate} from "./verifier.js";
 
 // TODO: Set bonuses
 export function minimizeRequiredSP(items, initialAssignedSP = [0, 0, 0, 0, 0]) {
     let optimalAssigned;
+    let optimalOrder;
     let minimumSPTotal = Infinity;
 
     const assignedSP = new Int32Array(5);
     const givenSP = new Int32Array(5);
+    const minimumSP = new Int32Array(5);
 
     function tryOrder(orderedIndexes) {
         assignedSP.set(initialAssignedSP);
         givenSP.fill(0);
+        minimumSP.fill(0);
         let totalSP = initialAssignedSP.reduce((a, b) => a + b);
 
         for (let i = 0; i < orderedIndexes.length; i++) {
@@ -34,17 +37,25 @@ export function minimizeRequiredSP(items, initialAssignedSP = [0, 0, 0, 0, 0]) {
                     }
             if (totalSP >= minimumSPTotal) return;
             // equip item
-            for (let j = 0; j < 5; j++) if (item.given[j] > 0)
-                givenSP[j] += item.given[j];
+            for (let j = 0; j < 5; j++) {
+                if (item.given[j] !== 0) givenSP[j] += item.given[j];
+                if (minimumSP[j] > 0 && assignedSP[j] + givenSP[j] < minimumSP[j]) assignedSP[j] = minimumSP[j] - givenSP[j];
+                if (item.cost[j] > 0) minimumSP[j] = Math.max(minimumSP[j], item.cost[j] - givenSP[j]);
+            }
         }
-        if (properValidate(assignedSP, items)) {
-            optimalAssigned = [...assignedSP];
-            minimumSPTotal = totalSP;
+
+        for (let i = 0; i < orderedIndexes.length; i++) {
+            const equippedItems = items.filter((x, j) => orderedIndexes.indexOf(j) <= i);
+            if (!wynnValidate(assignedSP, equippedItems)) return;
         }
+
+        optimalAssigned = [...assignedSP];
+        optimalOrder = [...orderedIndexes];
+        minimumSPTotal = totalSP;
     }
 
     // calls tryOrder with every possible ordering of 0 through items.length
     permuteOrders(items.length, items.length, tryOrder);
 
-    return {assignedSP: optimalAssigned};
+    return {assignedSP: optimalAssigned ?? [0, 0, 0, 0, 0], order: optimalOrder};
 }
