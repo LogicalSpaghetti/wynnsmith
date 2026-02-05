701420885bb67f59323da8bab6a5a5e77be48373
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2026-02-05 17:34:55 -0500

    Added base and ids to the automatic generation.

--- kotlin/src/TypeGeneration.kt
+++ kotlin/src/TypeGeneration.kt
@@ -1,22 +1,47 @@
 import api.getJSONFromAPI
 import file.writeStringToFile
+import org.json.JSONArray
 import org.json.JSONObject
 
+// TODO: make the generated file not have random ordering.
 fun typeGen() {
+    val data = JSONObject()
+
+    // metadata:
+
     val metadata = getJSONFromAPI("https://api.wynncraft.com/v3/item/metadata")
     val filters = metadata.getJSONObject("filters")
     val advanced = filters.getJSONObject("advanced")
     val itemType = filters.getJSONArray("type")
 
-    val data = JSONObject()
-
     data.put("item", itemType)
-    for (key in itemType.filter { it as String !== "tool" && advanced.has(it) }) {
+    for (key in itemType.filter { it as String !== "tool" && advanced.has(it) })
         data.put((key as String), advanced.getJSONArray(key))
-    }
 
     data.put("attackSpeed", advanced.getJSONArray("attackSpeed"))
     data.put("crafting", advanced.getJSONArray("crafting"))
+
+    // base/ids:
+
+    val itemAPI = getJSONFromAPI("https://api.wynncraft.com/v3/item/database?fullResult")
+
+    val base = JSONArray()
+    val ids = JSONArray()
+
+    for (key in itemAPI.names()) {
+        val item = itemAPI.getJSONObject(key as String)
+        if (item.has("identifications"))
+            item.getJSONObject("identifications").names()
+                .forEach { if (ids.indexOf(it as String) == -1) ids.put(it) }
+        if (item.has("base"))
+            item.getJSONObject("base").names()
+                .forEach { if (ids.indexOf(it as String) == -1) base.put(it) }
+    }
+
+    data.put("base", base)
+    data.put("identifications", ids)
+
+    // file creation:
 
     var str = "// File contents generated automatically in TypeGeneration.kt, do not modify.\n"
     for (key in data.names()) {
